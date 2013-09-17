9f03c22bc6409023318efd45221e3dc422c226b5
Rowan Crawford <wombleton@gmail.com>
2013-09-17 18:40:11 +1200

    use __dirname

--- rules/index.js
+++ rules/index.js
@@ -5,7 +5,7 @@ var _ = require('underscore'),
 
 require('js-yaml');
 
-files = fs.readdirSync('./data');
+files = fs.readdirSync(__dirname + '/../data');
 
 module.exports = {};
 
@@ -13,7 +13,7 @@ _.each(files, function(file) {
     var definitions;
 
     if (re.test(file)) {
-        definitions = require('../data/' + file);
+        definitions = require(__dirname + '/../data/' + file);
 
         _.each(definitions, function(definition) {
             _.each(definition.regions, function(locale) {
