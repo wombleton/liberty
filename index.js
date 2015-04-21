07d2b85a2551b88dadff05c1ee9fed3750920146
Bastian Krol <bastian.krol@web.de>
2015-04-21 12:25:49 +0200

    check if an array of locales has been passed

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ var _ = require('underscore'),
     liberty = require('./liberty');
 
 module.exports = function() {
-    this.locales = _.toArray(arguments);
-
+    if (arguments.length === 1 && _.isArray(arguments[0])) {
+        // use first (and only) argument as the array of locales
+        this.locales = _.toArray(arguments[0]);
+    } else {
+        // use variable length argument list as locales array
+        this.locales = _.toArray(arguments);
+    }
     _.extend(this, liberty);
 };
